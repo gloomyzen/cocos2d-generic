ca5e76615149a010df58c573fd51e347cc569703
Artur Barilo <faustkeller45@gmail.com>
2021-02-01 23:15:09 +0900

    (feature)master | wip: first screen -update ipa database and update find ipa

---
+++ utilityModule/stringUtility.h
@@ -0,0 +1,35 @@
+#ifndef CARDS_APP_STRINGUTILITY_H
+#define CARDS_APP_STRINGUTILITY_H
+
+#include <algorithm>
+#include <cctype>
+#include <locale>
+#include <string>
+
+namespace common {
+	namespace utilityModule {
+
+		class stringUtility {
+		public:
+			static inline std::string ltrim(std::string s) {
+				s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+					return !std::isspace(ch);
+				}));
+				return s;
+			}
+
+			static inline std::string rtrim(std::string s) {
+				s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+					return !std::isspace(ch);
+				}).base(), s.end());
+				return s;
+			}
+
+			static inline std::string trim(std::string s) {
+				return ltrim(rtrim(s));
+			}
+		};
+	}
+}
+
+#endif //CARDS_APP_STRINGUTILITY_H
