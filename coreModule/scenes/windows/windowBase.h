4ffb675c38fa76679ea6a423095e1205d3c1fdb6
Artur Barilo <faustkeller45@gmail.com>
2021-02-14 17:23:56 +0900

    (feature)master | wip window system - wip storing data

--- coreModule/scenes/windows/windowBase.h
+++ coreModule/scenes/windows/windowBase.h
@@ -3,15 +3,39 @@
 
 #include "cocos2d.h"
 #include "common/coreModule/nodes/nodeProperties.h"
+#include <string>
+#include <vector>
+#include <map>
 
 namespace common::coreModule {
 	class windowBase : public nodeProperties<cocos2d::Sprite> {
-//		template <typename T>
-//		class window
+		template <typename T>
+		class windowBaseData {
+		public:
+			std::vector<T> value;
+		};
+		class windowDataStorage {
+		public:
+			windowDataStorage() = default;
+			~windowDataStorage() = default;
+			template <typename T>
+			void setValue(const std::string& name, T data) {
+				//
+			}
+		};
 	public:
 		windowBase();
 		~windowBase();
 		CREATE_FUNC(windowBase);
+
+		template <typename T>
+		void setData (const std::string& name, T data){
+			auto storage = new windowDataStorage();
+			windowData[name] = *storage;
+			windowData[name].setValue<T>(name, data);
+		}
+	private:
+		std::map<std::string, windowDataStorage> windowData;
 	};
 }//common::coreModule
 
