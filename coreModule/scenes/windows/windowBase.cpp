68822cfff3d92ccc4f323add5f3de2f0969432fa
Artur Barilo <faustkeller45@gmail.com>
2021-02-18 01:36:42 +0900

    (feature)master | wip window system - wip course preview window - opening and closing actions

--- coreModule/scenes/windows/windowBase.cpp
+++ coreModule/scenes/windows/windowBase.cpp
@@ -8,7 +8,7 @@ using namespace cocos2d;
 windowBase::windowBase() {
 	initWindow();
 }
-windowBase::windowBase(std::string  name) : windowName(std::move(name)) {
+windowBase::windowBase(std::string name) : windowName(std::move(name)) {
 	initWindow();
 }
 
@@ -27,10 +27,33 @@ void windowBase::initWindow() {
 			closeWindow();
 		}
 	});
+
+	currentState = eWindowState::OPENING;
+	setScale(0.5f);
+	setOpacity(static_cast<uint8_t>(20));
+	setCascadeOpacityEnabled(true);
+	auto fadeTo = FadeTo::create(0.12f, 255.0f);
+	auto scaleBy = ScaleBy::create(0.12f, 2.0f);
+	auto spawn = Spawn::createWithTwoActions(scaleBy, fadeTo);
+	auto clb = cocos2d::CallFunc::create([this](){
+		currentState = eWindowState::OPENED;
+		setCascadeOpacityEnabled(false);
+	});
+	auto seq = Sequence::create(spawn, clb, nullptr);
+	runAction(seq);
 }
 
 void windowBase::closeWindow() {
-	auto clb = getData<std::function<void()>>("safeClose", [](){});
-	if (clb)
-		clb();
+	auto closeClb = getData<std::function<void()>>("safeClose", [](){});
+	currentState = eWindowState::CLOSING;
+	setCascadeOpacityEnabled(true);
+	auto fadeTo = FadeTo::create(0.08f, 0.0f);
+	auto scaleBy = ScaleBy::create(0.12f, 0.5f);
+	auto spawn = Spawn::createWithTwoActions(scaleBy, fadeTo);
+	auto clb = cocos2d::CallFunc::create([closeClb](){
+		if (closeClb)
+			closeClb();
+	});
+	auto seq = Sequence::create(spawn, clb, nullptr);
+	runAction(seq);
 }
