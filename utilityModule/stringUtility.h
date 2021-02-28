aafb8f9cc2729ff00bd6541c0f12d75d8747bce1
Artur Barilo <faustkeller45@gmail.com>
2021-03-01 01:13:31 +0900

    (feature)master | wip features with window system - wip examCard wnd examWindow - move methods to common

--- utilityModule/stringUtility.h
+++ utilityModule/stringUtility.h
@@ -3,14 +3,21 @@
 
 #include <algorithm>
 #include <cctype>
-#include <locale>
 #include <string>
+#include <clocale>
+#include <locale>
+#include <codecvt>
 
 namespace common {
 	namespace utilityModule {
 
 		class stringUtility {
 		public:
+			enum class eLocaleType {
+				EN = 0,
+				RU,
+			};
+
 			static inline std::string ltrim(std::string s) {
 				s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
 					return !std::isspace(ch);
@@ -28,6 +35,60 @@ namespace common {
 			static inline std::string trim(std::string s) {
 				return ltrim(rtrim(s));
 			}
+
+			static std::wstring utf8_to_wstring (const std::string& str) {
+				std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+				return conv.from_bytes(str);
+			}
+
+			static std::string wstring_to_utf8 (const std::wstring& str) {
+				std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+				return conv.to_bytes(str);
+			}
+
+			static std::string capitalizeString(const std::string &line, eLocaleType type = eLocaleType::EN) {
+				switch (type) {
+					case eLocaleType::EN: {
+						std::setlocale(LC_CTYPE, "en_US.UTF-8");
+						std::locale loc("en_US.UTF-8");
+					}
+						break;
+					case eLocaleType::RU: {
+						std::setlocale(LC_CTYPE, "ru_RU.UTF-8");
+						std::locale loc("ru_RU.UTF-8");
+					}
+						break;
+				}
+				auto wstr = utf8_to_wstring(line);
+				for (auto it = wstr.begin(); it != wstr.end(); ++it) {
+					if (it == wstr.begin()) {
+						*it = std::toupper(*it);
+					} else {
+						*it = std::tolower(*it);
+					}
+				}
+				return wstring_to_utf8(wstr);
+			};
+
+			static std::string toLowerString(const std::string &line, eLocaleType type = eLocaleType::EN) {
+				switch (type) {
+					case eLocaleType::EN: {
+						std::setlocale(LC_CTYPE, "en_US.UTF-8");
+						std::locale loc("en_US.UTF-8");
+					}
+						break;
+					case eLocaleType::RU: {
+						std::setlocale(LC_CTYPE, "ru_RU.UTF-8");
+						std::locale loc("ru_RU.UTF-8");
+					}
+						break;
+				}
+				auto wstr = utf8_to_wstring(line);
+				for (auto it = wstr.begin(); it != wstr.end(); ++it) {
+					*it = std::tolower(*it);
+				}
+				return wstring_to_utf8(wstr);
+			};
 		};
 	}
 }
