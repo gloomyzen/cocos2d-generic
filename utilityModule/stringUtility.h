615d6272f42752a34b54af35f77fa6f242e9cd10
Artur Barilo <faustkeller45@gmail.com>
2021-03-04 22:33:35 +0900

    (feature)master | wip features with window system - wip examCard wnd examWindow - remove requests - remove std::locale from string utility

--- utilityModule/stringUtility.h
+++ utilityModule/stringUtility.h
@@ -7,90 +7,57 @@
 #include <clocale>
 #include <locale>
 #include <codecvt>
+#include <utility>
 
-namespace common {
-	namespace utilityModule {
+namespace common::utilityModule {
+	class stringUtility {
+	public:
+		static inline std::string ltrim(std::string s) {
+			s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+				return !std::isspace(ch);
+			}));
+			return s;
+		}
 
-		class stringUtility {
-		public:
-			enum class eLocaleType {
-				EN = 0,
-				RU,
-			};
+		static inline std::string rtrim(std::string s) {
+			s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+				return !std::isspace(ch);
+			}).base(), s.end());
+			return s;
+		}
 
-			static inline std::string ltrim(std::string s) {
-				s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-					return !std::isspace(ch);
-				}));
-				return s;
-			}
+		static inline std::string trim(std::string s) {
+			return ltrim(rtrim(std::move(s)));
+		}
 
-			static inline std::string rtrim(std::string s) {
-				s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-					return !std::isspace(ch);
-				}).base(), s.end());
-				return s;
-			}
+		static std::wstring utf8_to_wstring(const std::string &str) {
+			std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+			return conv.from_bytes(str);
+		}
 
-			static inline std::string trim(std::string s) {
-				return ltrim(rtrim(s));
-			}
+		static std::string wstring_to_utf8(const std::wstring &str) {
+			std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+			return conv.to_bytes(str);
+		}
 
-			static std::wstring utf8_to_wstring (const std::string& str) {
-				std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-				return conv.from_bytes(str);
-			}
-
-			static std::string wstring_to_utf8 (const std::wstring& str) {
-				std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-				return conv.to_bytes(str);
-			}
-
-			static std::string capitalizeString(const std::string &line, eLocaleType type = eLocaleType::EN) {
-				switch (type) {
-					case eLocaleType::EN: {
-						std::setlocale(LC_CTYPE, "en_US.UTF-8");
-						std::locale loc("en_US.UTF-8");
-					}
-						break;
-					case eLocaleType::RU: {
-						std::setlocale(LC_CTYPE, "ru_RU.UTF-8");
-						std::locale loc("ru_RU.UTF-8");
-					}
-						break;
-				}
-				auto wstr = utf8_to_wstring(line);
-				for (auto it = wstr.begin(); it != wstr.end(); ++it) {
-					if (it == wstr.begin()) {
-						*it = std::toupper(*it);
-					} else {
-						*it = std::tolower(*it);
-					}
-				}
-				return wstring_to_utf8(wstr);
-			};
-
-			static std::string toLowerString(const std::string &line, eLocaleType type = eLocaleType::EN) {
-				switch (type) {
-					case eLocaleType::EN: {
-						std::setlocale(LC_CTYPE, "en_US.UTF-8");
-						std::locale loc("en_US.UTF-8");
-					}
-						break;
-					case eLocaleType::RU: {
-						std::setlocale(LC_CTYPE, "ru_RU.UTF-8");
-						std::locale loc("ru_RU.UTF-8");
-					}
-						break;
-				}
-				auto wstr = utf8_to_wstring(line);
-				for (auto it = wstr.begin(); it != wstr.end(); ++it) {
+		static std::string capitalizeString(std::string line) {
+			for (auto it = line.begin(); it != line.end(); ++it) {
+				if (it == line.begin()) {
+					*it = std::toupper(*it);
+				} else {
 					*it = std::tolower(*it);
 				}
-				return wstring_to_utf8(wstr);
-			};
+			}
+			return line;
 		};
-	}
+
+		static std::string toLowerString(std::string line) {
+			for (auto it = line.begin(); it != line.end(); ++it) {
+				*it = std::tolower(*it);
+			}
+			return line;
+		};
+	};
 }
 
 #endif //COMMON_STRINGUTILITY_H
