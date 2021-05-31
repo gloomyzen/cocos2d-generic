f50c4e3a631f6c6baaefa83432af50c9c6716a61
Artur Barilo <faustkeller45@gmail.com>
2021-05-31 21:23:00 +0300

    master | signals

--- coreModule/signals/signalHolder.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_SIGNALHOLDER_H
-#define COMMON_SIGNALHOLDER_H
-
-namespace common::coreModule {
-    class signalHolder {};
-}// namespace common
-
-#endif// COMMON_SIGNALHOLDER_H
--- coreModule/signals/signalInstance.h
+++ coreModule/signals/signalInstance.h
@@ -4,10 +4,22 @@
 #include <map>
 #include <vector>
 #include <tuple>
+#include <functional>
 
 #define GET_SIGNALS() common::coreModule::signalInstance::getInstance()
 
 namespace common::coreModule {
+
+    template<typename ...Types>
+    class signalHolder {
+    public:
+        void emit(Types... args) {}
+        bool operator==(const signalHolder& data) const {
+            return data.id == this->id;
+        }
+        unsigned int id;
+    };
+
     class signalBaseData {
     public:
         signalBaseData() = default;
@@ -18,12 +30,14 @@ namespace common::coreModule {
     class TypedSignalBaseData : public signalBaseData {
     public:
         explicit TypedSignalBaseData(std::tuple<Types...> data) : signalBaseData(), m_data(data) {}
-        std::tuple<Types...> getData() {
+        std::tuple<Types...> getAllData() {
             return m_data;
         }
 
     private:
         std::tuple<Types...> m_data;
+        signalHolder<Types...>* signal;
+        std::vector<std::function<void(Types...)>> listeners;
     };
 
     class signalInstance {
@@ -33,7 +47,13 @@ namespace common::coreModule {
         static signalInstance& getInstance();
         void clean();
 
+        template<typename ...Types>
+        void addSignal(signalHolder<Types...>* signal) {
+            //
+        }
+
     private:
+        unsigned int signalId = 0u;
 //        std::map<>
     };
 }
