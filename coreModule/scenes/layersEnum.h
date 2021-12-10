5796a5d980652cdf888a5040803ac2a870b1b0d9
Artur Barilo <faustkeller45@gmail.com>
2021-12-10 21:42:06 +0300

    master | wip: rework scene logic

--- coreModule/scenes/layersEnum.h
+++ coreModule/scenes/layersEnum.h
@@ -1,16 +1,12 @@
 #ifndef GENERIC_LAYERSENUM_H
 #define GENERIC_LAYERSENUM_H
 
-namespace generic {
-
-    namespace coreModule {
-
-        enum eGameLayers {
-            ROOT = 0,
-            WINDOW = 20,
-            DEBUG_LAYER = 99,
-        };
-    }
-}// namespace generic
+namespace generic::coreModule {
+    enum eGameLayers {
+        ROOT = 0,
+        WINDOW = 90,
+        DEBUG_LAYER = 99,
+    };
+}// namespace generic::coreModule
 
 #endif// GENERIC_LAYERSENUM_H
