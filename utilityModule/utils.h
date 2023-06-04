022ff18f46ccf1b7449e062a35aa1aedffecd09c
Artur Barilo <efreyu@gmail.com>
2023-06-04 22:22:43 +0300

    master | update magic enum and ftm support

---
+++ utilityModule/utils.h
@@ -0,0 +1,9 @@
+#ifndef MATCH3_GAME_UTILS_H
+#define MATCH3_GAME_UTILS_H
+
+#include "generic/utilityModule/stringUtility.h"
+#include "generic/utilityModule/logManager.h"
+#include "generic/utilityModule/findUtility.h"
+#include "generic/utilityModule/randomUtility.h"
+
+#endif// MATCH3_GAME_UTILS_H
